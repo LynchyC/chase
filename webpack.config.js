a670c4962ece3ea46e35b0597cff609a8de3ecef
Charlie Lynch <lynchcoding@gmail.com>
2018-05-27 21:28:31 +0100

    Add 'HTMLWebpackPlugin' and 'CleanWebpackPlugin' to webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const commonConfig = {
     output: {
@@ -23,7 +25,18 @@ const commonConfig = {
     },
     resolve: {
         extensions: ['.ts', '.js']
-    }
+    },
+    node: {
+        __dirname: false
+    },
+    plugins: [
+        new CleanWebpackPlugin('dist'),
+        new HtmlWebpackPlugin({
+            title: 'Chase',
+            template: './src/index.html',
+            inject: false
+        })
+    ]
 };
 
 module.exports = [
