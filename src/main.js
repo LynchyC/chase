328f78e9f6278c5329ec19da16cd1bbed37646d1
Charlie Lynch <lynchcoding@gmail.com>
2019-04-12 20:33:14 +0100

    Add file to watch list when accessing from 'Open File' menu option

--- src/main.js
+++ src/main.js
@@ -1,103 +1,109 @@
 import { app, BrowserWindow, dialog, ipcMain, Menu } from "electron";
-import { join } from "path";
+import { basename, join } from "path";
 import { format } from "url";
 import Watcher from "./watcher";
+
 const isDevelopment = process.env.NODE_ENV === "development" ? true : false;
 
 let mainWindow;
 let watcher;
 
 function createWindow() {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
-    minHeight: 515,
-    minWidth: 465,
-    webPreferences: {
-      backgroundThrottling: false,
-    },
-  });
+	// Create the browser window.
+	mainWindow = new BrowserWindow({
+		minHeight: 515,
+		minWidth: 465,
+		webPreferences: {
+			backgroundThrottling: false,
+			nodeIntegration: true
+		},
+	});
 
-  watcher = new Watcher(mainWindow);
+	watcher = new Watcher(mainWindow);
 
-  // and load the index.html of the app.
-  if (isDevelopment) {
-    mainWindow.loadURL("http://localhost:8080");
-  } else {
-    mainWindow.loadURL(format({
-      pathname: join(__dirname, "index.html"),
-      protocol: "file",
-      slashes: true,
-    }));
-  }
+	// and load the index.html of the app.
+	if (isDevelopment) {
+		mainWindow.loadURL("http://localhost:8080");
+	} else {
+		mainWindow.loadURL(format({
+			pathname: join(__dirname, "index.html"),
+			protocol: "file",
+			slashes: true,
+		}));
+	}
 
-  mainWindow.on("closed", () => {
-    mainWindow = null;
-  });
+	mainWindow.on("closed", () => {
+		mainWindow = null;
+	});
 }
 
 app.on("ready", () => {
-  createWindow();
+	createWindow();
 
-  let template = [{
-    label: "File",
-    submenu: [{
-      label: "Open File",
-      click: () => {
-        dialog.showOpenDialog(mainWindow, {
-          title: "Open File",
-          properties: ["openFile"],
-        });
-      },
-      accelerator: "CmdOrCtrl+O",
-    }, {
-      role: "close",
-      accelerator: "CmdOrCtrl+Q",
-    }],
-  }];
+	let template = [{
+		label: "File",
+		submenu: [{
+			label: "Open File",
+			click: () => {
+				dialog.showOpenDialog(mainWindow, {
+					filters: [{
+						name: 'Files',
+						filters: ['doc', 'docx', 'html', 'htm', 'odt', 'pdf', 'xls', 'xlsx', 'ods', 'ppt', 'pptx', 'txt', 'log']
+					}],
+					title: "Open File",
+					properties: ["openFile"],
+				}, ([file]) => watcher.add(basename(file), file));
+			},
+			accelerator: "CmdOrCtrl+O",
+		}, {
+			role: "close",
+			accelerator: "CmdOrCtrl+Q",
+		}],
+	}];
 
-  if (isDevelopment) {
-    template = [...template, {
-      label: "Developer",
-      submenu: [{
-        label: "Open Dev Tools",
-        click() {
-          if (mainWindow.webContents.isDevToolsOpened()) {
-            mainWindow.webContents.closeDevTools();
-          } else {
-            mainWindow.webContents.openDevTools();
-          }
-        },
-        accelerator: "CmdOrCtrl+Shift+F12",
-      }, {
-        label: "Force Reload",
-        click() {
-          mainWindow.reload();
-        },
-        accelerator: "CmdOrCtrl+Shift+R",
-      }],
-    }];
-  }
+	if (isDevelopment) {
+		template = [...template, {
+			label: "Developer",
+			submenu: [{
+				label: "Open Dev Tools",
+				click() {
+					if (mainWindow.webContents.isDevToolsOpened()) {
+						mainWindow.webContents.closeDevTools();
+					} else {
+						mainWindow.webContents.openDevTools();
+					}
+				},
+				accelerator: "CmdOrCtrl+Shift+F12",
+			}, {
+				label: "Force Reload",
+				click() {
+					mainWindow.reload();
+				},
+				accelerator: "CmdOrCtrl+Shift+R",
+			}],
+		}];
+	}
 
-  const menu = Menu.buildFromTemplate(template);
-  Menu.setApplicationMenu(menu);
+	const menu = Menu.buildFromTemplate(template);
+	Menu.setApplicationMenu(menu);
 });
 
 app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
+	if (process.platform !== "darwin") {
+		app.quit();
+	}
 });
 
 app.on("activate", () => {
-  if (mainWindow === null) {
-    createWindow();
-  }
+	if (mainWindow === null) {
+		createWindow();
+	}
 });
 
 ipcMain.on("file:added", (e, name, path) => {
-  watcher.add(name, path);
+	watcher.add(name, path);
 });
 
 ipcMain.on("file:removed", (e, id) => {
-  watcher.remove(id);
+	watcher.remove(id);
 });
