b4c1ad794a7475669e2e6fabc79ad246c81e69de
Charlie Lynch <lynchcoding@gmail.com>
2019-04-15 00:19:04 +0100

    Set up webpack path aliases

--- src/utils/ipcManager.js
+++ src/utils/ipcManager.js
@@ -1,5 +1,5 @@
 import { ipcRenderer } from "electron";
-import { addFileSuccess, removeFileSuccess } from "../actions/watchlist";
+import { addFileSuccess, removeFileSuccess } from "actions/watchlist";
 
 export default class IPCManager {
 
