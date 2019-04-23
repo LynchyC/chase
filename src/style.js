6fb29d9411648d7fb97048137e4c1de9f40d986a
Charlie Lynch <lynchcoding@gmail.com>
2019-04-23 21:21:26 +0100

    Apply Roboto font to application

--- src/style.js
+++ src/style.js
@@ -1,13 +1,19 @@
 import { createGlobalStyle } from "styled-components";
+import RobotoFont from "fonts/Roboto/Roboto-Regular.ttf";
 
 export default createGlobalStyle`
     html, body, #app {
         background-image: linear-gradient(to bottom, #A9A9A9, #C0C0C0);
         color: white;
-        font-family: Arial, Helvetica, sans-serif;
+        font-family: Roboto, sans-serif;
         height: 100%;
         margin: 0;
         width: 100%;
+    }
+    
+    @font-face {
+        font-family: "Roboto";
+        src: url(${RobotoFont}) format("truetype");
     }
     
     * {
