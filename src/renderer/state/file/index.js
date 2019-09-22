5fb49f669db333993e2f3ba6e421877b894ff3d9
Charlie Lynch <lynchcoding@gmail.com>
2019-09-22 18:52:05 +0100

    Re-implement saving scrollTop of log views

--- src/renderer/state/file/index.js
+++ src/renderer/state/file/index.js
@@ -1,7 +1,7 @@
-import { ADD_FILE, FOLLOW_FILE, REMOVE_FILE, UPDATE_FILE } from "renderer/constants";
+import { ADD_FILE, FOLLOW_FILE, REMOVE_FILE, SET_SCROLL, UPDATE_FILE } from "renderer/constants";
 
 export default (state = {}, action) => {
-    const { file, id, type } = action;
+    const { file, id, scrollTop, type } = action;
     switch (type) {
         case ADD_FILE:
             return {
@@ -17,12 +17,21 @@ export default (state = {}, action) => {
                 ...state,
                 [id]: {
                     ...state[id],
-                    follow: !state[id].follow
+                    follow: !state[id].follow,
+                    scrollTop
                 }
             };
         case REMOVE_FILE:
             const { [id]: toDelete, ...files } = state;
             return files;
+        case SET_SCROLL:
+            return {
+                ...state,
+                [id]: {
+                    ...state[id],
+                    scrollTop
+                }
+            };
         case UPDATE_FILE:
             return {
                 ...state,
