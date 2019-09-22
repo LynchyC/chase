144052daec5b60cdaf76aa0747ec41081dc11440
Charlie Lynch <lynchcoding@gmail.com>
2019-09-22 19:24:08 +0100

    Rename 'log-view' folder

--- src/renderer/views/index.js
+++ src/renderer/views/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from "redux";
 
-import log from "renderer/views/log-view/reducers";
+import log from "renderer/views/log/reducers";
 
 export default combineReducers({
     log
