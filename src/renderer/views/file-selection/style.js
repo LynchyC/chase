0be92283d06560828603dd65f2dd3a531df1f2b5
Charlie Lynch <lynchcoding@gmail.com>
2020-02-22 21:09:18 +0000

    Apply View styling to FileSelection

--- src/renderer/views/file-selection/style.js
+++ src/renderer/views/file-selection/style.js
@@ -1,46 +1,26 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
-    height: 100vh;
-    overflow: hidden;
-    padding: 0 5%;
-    width: 100%;
-`;
+import View from "../../components/view";
 
-export const DropzoneHeader = styled.h1`
-    text-align: center;
-    font-size: 1rem;
-
-    @media screen and (min-height: 480px) and (min-width: 520px) {
-        font-size: 2.5rem;
-    }
-`;
-
-export const Header = styled.div`
+export const Area = styled(View)`
     align-items: center;
-    display: flex;
-    height: 10vh;
-    justify-content: center;
-    margin-bottom: 1vh;
-`;
-
-export const HeaderText = styled.h1`
-  margin: 0;
-`;
-
-export const StyledDropzone = styled.div`
-    align-items: center;
-    background-color: ${({ isActive }) => isActive && "#1D2F49"};
-    border: 4px dashed white;
+    border: 6px dashed white;
     border-radius: 5px;
     cursor: pointer;
-    display: flex;
-    height: 85vh;
+    flex-grow: 1;
     justify-content: center;
-    outline: 0;
+    outline: none;
     user-select: none;
+`;
 
-    @media screen and (min-height: 480px) and (min-width: 520px) {
-        border: 8px dashed white;
-    }
+export const Container = styled(View)`
+    flex-grow: 1;
+    height: 100%;
+    justify-content: center;
+    padding: 5%;
+`;
+
+export const Text = styled(View)`
+    text-align: center;
+    font-size: 30px;
 `;
