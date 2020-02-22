365f48d7f3b929ebcef03e7784e8900450a68a50
Charlie Lynch <lynchcoding@gmail.com>
2020-02-22 22:57:17 +0000

    Add styling to Tab, update global styling

--- src/renderer/components/tabs/style.js
+++ src/renderer/components/tabs/style.js
@@ -1,27 +1,25 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
-    height: 100%;
-    width: 100%;
-    padding: 1%;
+import View from "../view";
+
+export const Container = styled(View)`
+    flex-grow: 1;
 `;
 
-export const TabsList = styled.ul`
+export const TabsList = styled(View)`
     border-bottom: 0.016rem solid #808080;
-    display: flex;
+    flex-direction: row;
     flex-wrap: nowrap;
     height: 2.5rem;
     list-style: none;
     margin: 0;
     overflow-x: auto;
     padding: 0;
-    
     ::-webkit-scrollbar {
         display: none;
     }
 `;
 
-export const TabBody = styled.div`
-    height: calc(100% - 2.5rem);
-    max-height: 100%;
+export const TabBody = styled(View)`
+    flex-grow: 1;
 `;
