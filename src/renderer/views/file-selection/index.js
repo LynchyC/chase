f54bcd1a86da023822fbe02fa9dc8b866a73be23
Charlie Lynch <lynchcoding@gmail.com>
2020-05-31 20:20:54 +0100

    Integrate Dropzone into FIle-Selection component

--- src/renderer/views/file-selection/index.js
+++ src/renderer/views/file-selection/index.js
@@ -1,13 +1,44 @@
+import { array } from "prop-types";
+import React, { Fragment, useEffect } from "react";
 import { connect } from "react-redux";
+import { useHistory } from "react-router-dom";
 import { createStructuredSelector } from "reselect";
 
-import FileSelection from "./component";
-import { add } from "../../state/file/actions";
 import { files } from "../../state/file/selectors";
+import { Container, Dropzone, Text } from "./style";
+
+const FileSelection = ({ files }) => {
+    const { push } = useHistory();
+
+    useEffect(() => {
+        if (files.length) {
+            push("/logs");
+        }
+    }, [files]);
+
+    return <Container>
+        <Dropzone>
+            {({ getInputProps, isDragActive, isDragReject }) => {
+                return <Fragment>
+                    <input {...getInputProps()} />
+                    <Text>
+                        {!isDragActive && !isDragReject && "Drop a file here to get started or click me!"}
+                        {isDragActive && !isDragReject && "This file is authorized"}
+                        {isDragActive && isDragReject && "Sorry, this file is not authorized ..."}
+                    </Text>
+                </Fragment>
+            }}
+        </Dropzone>
+    </Container>
+};
+
+
+FileSelection.propTypes = {
+    files: array.isRequired
+};
 
 export default connect(
     createStructuredSelector({
         files
-    }), {
-    add
-})(FileSelection);
+    })
+)(FileSelection);
